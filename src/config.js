a49befa9bc7ec81a8a5920f5c1d22348320a083f
sinsys <nicholas.hazel@gmail.com>
2020-03-31 18:36:34 -0700

    Setup database schema. Ran migration. Seeded successfully.

--- src/config.js
+++ src/config.js
@@ -3,4 +3,8 @@ require('dotenv').config();
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://amazement:test@localhost/amazement_test',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://amazement:test@localhost/amazement',
+  JWT_SECRET: process.env.JWT_SECRET || 'dev-secret',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "https://amazement.now.sh"
 }
